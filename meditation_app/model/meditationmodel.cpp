03f7457d582cb314015a95e08bf821809e9646be
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2020-12-20 23:50:05 +0100

    Implemented MeditationModel

--- meditation_app/model/meditationmodel.cpp
+++ meditation_app/model/meditationmodel.cpp
@@ -1,6 +1,77 @@
 #include "meditationmodel.h"
+#include <QDebug>
 
-MeditationModel::MeditationModel(QObject *parent) : QObject(parent)
-{
+MeditationModel::MeditationState
+MeditationModel::toModelMeditationState(const med::MeditationState state) {
+    switch (state) {
+        case med::MeditationState::STARTED: return MeditationModel::MeditationState::Started;
+        case med::MeditationState::PAUSED: return MeditationModel::MeditationState::Paused;
+        case med::MeditationState::STOPPED: return MeditationModel::MeditationState::Stopped;
+        case med::MeditationState::IDLE: return MeditationModel::MeditationState::Idle;
+    }
+    return MeditationModel::MeditationState::Idle;
+}
 
+MeditationModel::MeditationModel(QObject* parent) : QObject(parent) {}
+
+MeditationModel::MeditationState MeditationModel::getState() const {
+    return state_;
+}
+
+uint16_t MeditationModel::getDuration() const {
+    return config_.duration_s;
+}
+
+uint16_t MeditationModel::getTimeLeft() const {
+    return time_left_s_;
+}
+
+void MeditationModel::setDuration(uint16_t duration) {
+    if (config_.duration_s == duration) {
+        qDebug() << "MeditationModel::setDuration-> no change, returning";
+        return;
+    }
+    qDebug() << "MeditationModel::setDuration-> change from "
+             << static_cast<int>(config_.duration_s) << " to " << static_cast<int>(duration);
+
+    config_.duration_s = duration;
+    emit durationChanged(duration);
+    emit configChanged(config_);
+}
+
+void MeditationModel::setState(med::MeditationState state) {
+    const auto model_state = toModelMeditationState(state);
+    if (state_ == model_state) {
+        qDebug() << "MeditationModel::stateChanged-> no change, returning";
+    }
+
+    qDebug() << "MeditationModel::setDuration-> change from " << static_cast<int>(state_) << " to "
+             << static_cast<int>(model_state);
+
+    state_ = model_state;
+    emit modelStateChanged(state_);
+}
+
+void MeditationModel::setTimeLeft(uint16_t time_left_s) {
+    if (time_left_s_ == time_left_s) {
+        qDebug() << "MeditationModel::setTimeLeft-> no change, returning";
+    }
+
+    qDebug() << "MeditationModel::setTimeLeft-> change from " << time_left_s_ << " to "
+             << time_left_s;
+
+    time_left_s_ = time_left_s;
+    emit timeLeftChanged(time_left_s_);
+}
+
+void MeditationModel::startMeditation() {
+    emit meditationStarted();
+}
+
+void MeditationModel::pauseMeditation() {
+    emit meditationPaused();
+}
+
+void MeditationModel::stopMeditation() {
+    emit meditationStopped();
 }
--- meditation_app/model/meditationmodel.h
+++ meditation_app/model/meditationmodel.h
@@ -1,16 +1,62 @@
 #ifndef MEDITATIONMODEL_H
 #define MEDITATIONMODEL_H
 
+#include "logic/meditationconfig.h"
+#include "logic/meditationstate.h"
 #include <QObject>
 
 class MeditationModel : public QObject
 {
     Q_OBJECT
-public:
-    explicit MeditationModel(QObject *parent = nullptr);
+    Q_PROPERTY(MeditationState state READ getState NOTIFY modelStateChanged)
+    Q_PROPERTY(uint16_t duration READ getDuration WRITE setDuration NOTIFY durationChanged)
+    Q_PROPERTY(uint16_t timeLeftS READ getTimeLeft WRITE setTimeLeft NOTIFY timeLeftChanged)
 
-signals:
+  public:
+    enum class MeditationState {
+        Started,
+        Paused,
+        Stopped,
+        Idle,
+    };
+    Q_ENUM(MeditationState);
 
+    static MeditationState toModelMeditationState(const med::MeditationState state);
+
+    explicit MeditationModel(QObject* parent = nullptr);
+
+    MeditationState getState() const;
+    uint16_t getDuration() const;
+    uint16_t getTimeLeft() const;
+    void setDuration(uint16_t duration);
+
+  signals:
+    // used by QML
+    void modelStateChanged(MeditationState state);
+    void durationChanged(uint16_t duration);
+    void timeLeftChanged(uint16_t timeLeftChanged);
+
+    // used by C++
+    void configChanged(med::MeditationConfig config);
+
+    void meditationStarted();
+    void meditationPaused();
+    void meditationStopped();
+
+  public slots:
+    // used by C++
+    void setState(med::MeditationState state);
+    void setTimeLeft(uint16_t time_left_s);
+
+    // used by QML
+    void startMeditation();
+    void pauseMeditation();
+    void stopMeditation();
+
+  private:
+    MeditationState state_;
+    med::MeditationConfig config_;
+    uint16_t time_left_s_;
 };
 
 #endif // MEDITATIONMODEL_H
