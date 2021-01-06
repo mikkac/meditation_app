00d938eec5b35d2b4fbb35cff37409f2d2dc2831
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2021-01-06 01:36:58 +0100

    changed default value of duration

--- meditation_app/logic/meditationconfig.h
+++ meditation_app/logic/meditationconfig.h
@@ -6,8 +6,8 @@
 namespace med
 {
 struct MeditationConfig {
-    uint16_t duration_s{600};
-    uint16_t time_left_s{600};
+    uint16_t duration_s{6000};
+    uint16_t time_left_s{6000};
 };
 
 } // namespace med
